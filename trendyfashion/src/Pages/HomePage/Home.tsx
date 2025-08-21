13766b4dba450b573beea4afce322aa85ebfc5db
Amarachi-Codes <etheldorisamarachi@gmail.com>
2025-08-21 14:52:28 +0100

    updated navbar

--- trendyfashion/src/Pages/HomePage/Home.tsx
+++ trendyfashion/src/Pages/HomePage/Home.tsx
@@ -1,11 +1,12 @@
 
 import Hero from "../../assets/hero.jpg";
+import Navbar from "../../components/Navbar/Navbar";
 
 
 const Home = () => {
   return (
     <div>
-        hi
+       <Navbar/>
       <img src={Hero} alt=""  />
     </div>
   )
