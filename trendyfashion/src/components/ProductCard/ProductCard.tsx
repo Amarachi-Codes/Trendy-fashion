5bec48ca207498ef11e529baee256b7028d22591
Amarachi-Codes <etheldorisamarachi@gmail.com>
2025-09-06 14:05:30 +0100

    updated

--- trendyfashion/src/components/ProductCard/ProductCard.tsx
+++ trendyfashion/src/components/ProductCard/ProductCard.tsx
@@ -1,24 +1,46 @@
-
+import { NavLink } from "react-router-dom";
 
 const ProductCard = () => {
   return (
     <div>
       <div>
         <div>
-
+          <div>
+            <p>Category</p>
+            <ul>
+              <NavLink to={""}>Womens Clothing</NavLink>
+              <NavLink to={""}>Mens Clothing</NavLink>
+              <NavLink to={""}>Jewelry & Accessories</NavLink>
+              <NavLink to={""}>Bags & Shoes</NavLink>
+              <NavLink to={""}>Mother & Kids</NavLink>
+            </ul>
+          </div>
+          <div>
+            <p>Fill by price</p>
+            <ul>
+              <p>Range</p>
+              <p>Color</p>
+              <p>Item Condition</p>
+              <button></button>
+            </ul>
+          </div>
+          <div>
+            <p>New Product</p>
+            <ul>
+              <p>Range</p>
+              <p>Color</p>
+              <p>Item Condition</p>
+              <button></button>
+            </ul>
+          </div>
         </div>
         <div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
+          <div></div>
+          <div></div>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
